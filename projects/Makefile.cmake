e240fd1fd747954f4c7aaf7c7188dcaeb8379b88
Rahul Gopinath <rahul@puppetlabs.com>
2014-08-20 11:50:49 -0700

    Revert "Update for building"

--- projects/Makefile.cmake
+++ projects/Makefile.cmake
@@ -14,7 +14,6 @@ $(installroot)/$(arch)/$(cmaketoolchain): patches/sol-VER-ARCH-toolchain.cmake
 		-e 's#%VER%#5.$(sys_rel)#g' \
 		-e 's#%ARCH%#$(arch)#g' \
 		-e 's#%TARGET%#$(target)#g' \
-		-e 's#%PREFIX%#$(prefix)#g' \
 		> $@
 
 build/$(arch)/$(cmake_)/._.config: install/$(arch)/gcc-$(gcc_ver)/._.install
@@ -45,7 +44,6 @@ cmakeenv: $(installroot)/$(arch)/$(cmaketoolchain)
 		-e 's#%VER%#5.$(sys_rel)#g' \
 		-e 's#%ARCH%#$(arch)#g' \
 		-e 's#%TARGET%#$(target)#g' \
-		-e 's#%PREFIX%#$(prefix)#g' \
 		> $<
 
 	@echo $@ done
